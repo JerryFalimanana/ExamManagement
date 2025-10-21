7ff8e9109ee5bc04d1b5421aa4786e964197eb93
Jerry Falimanana <jerry@adrware.mg>
2025-10-21 16:01:51 +0300

    remove DialogService

--- angular-front/src/app/services/exam-service.ts
+++ angular-front/src/app/services/exam-service.ts
@@ -1,21 +1,11 @@
 import { Injectable } from '@angular/core';
-import { DialogService, DynamicDialogRef } from 'primeng/dynamicdialog';
-import { CreateModal } from '../components/exam/create-modal/create-modal';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExamService {
     constructor(
-        private dialogService: DialogService,
+      
     ) {}
-
-    openCreationModal() {
-        return this.dialogService.open(CreateModal, {
-            styleClass: '',
-            width: '60%',
-            header: 'Organiser un examen',
-            baseZIndex: 10000
-        });
-    }
+    
 }
