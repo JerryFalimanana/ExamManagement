a4fe498a2439d1469de32ffae71b0df73cead8e6
Jerry Falimanana <jerry@adrware.mg>
2025-10-22 00:44:27 +0300

    custom create exam modal

--- angular-front/src/app/components/exam/create-modal/create-modal.ts
+++ angular-front/src/app/components/exam/create-modal/create-modal.ts
@@ -13,4 +13,5 @@ import { CommonModule } from '@angular/common';
 export class CreateModal {
     @Output() confirm = new EventEmitter<void>();
     @Output() cancel = new EventEmitter<void>();
+    today = new Date().toISOString().split('T')[0];
 }
