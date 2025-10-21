8200906bd3f931bb9a30aef1c6e3ebcfc744d637
Jerry Falimanana <jerry@adrware.mg>
2025-10-21 08:49:22 +0300

    build exam list static

--- angular-front/src/app/app.routes.ts
+++ angular-front/src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
-import { Login } from './login/login';
+import { Login } from './components/login/login';
+import { Exam } from './components/exam/exam';
 
 export const routes: Routes = [
     {
@@ -10,5 +11,9 @@ export const routes: Routes = [
     {
         path: 'login',
         component: Login
+    },
+    {
+        path: 'exams',
+        component: Exam
     }
 ];
